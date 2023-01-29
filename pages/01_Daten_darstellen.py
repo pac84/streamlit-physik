3433bc07bb213fc303816556775758e8c8973f0d
m-pagel <pagel@max-planck-gymnasium.de>
2023-01-29 22:43:13 +0100

    fit

--- pages/01_Daten_darstellen.py
+++ pages/01_Daten_darstellen.py
@@ -2,6 +2,7 @@ import streamlit as st
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 st.set_page_config(
     page_title="Physik",
@@ -53,28 +54,60 @@ try:
         ax.scatter(x_werte, y_werte)
         #legende.append('Datensatz %d' % (i+1))
 
-        if zeichne_fit:
-            if i==0:
-                st.markdown('### Grad der Polynome')
-            bezeichnung_grad = 'Grad des Polynoms für Anpassung von Datensatz %d' % (i+1)
-            print(bezeichnung_grad)
-            grad = st.slider(bezeichnung_grad, 1, 5, 1)
+    if zeichne_fit:
+        anpassung = st.radio("Art der Funktionsanpassung", ('ganzrationale Funktion', 'Exponentialfunktion', 'Trigonometrische Funktion'))
+        for i in range(len(eingabe_x_werte)):
+            x_werte = np.fromstring(eingabe_x_werte[i], dtype=float, sep=',')
+            y_werte = np.fromstring(eingabe_y_werte[i], dtype=float, sep=',')
             x_s=np.arange(np.amin(x_werte), np.amax(x_werte),(np.amax(x_werte)-np.amin(x_werte))/1000)
-
-            model=np.polyfit(x_werte,y_werte,grad)
-            model_f=np.poly1d(model)
-
-            ax.plot(x_s,model_f(x_s))
 
             x = sp.symbols('x')
             f = sp.Function('f')
-            f = 0*x
-            zaehler = 0
 
-            for zaehler in range(len(model)):
-                summand = sp.parse_expr("%f * x**(%d)" % (model[zaehler], len(model)-zaehler-1))
-                f += summand
-            funktionen.append(r'f_{%d}(x) = ' % (i+1) + sp.latex(f))
+            if anpassung == "ganzrationale Funktion":
+                bezeichnung_grad = 'Grad des Polynoms für Anpassung von Datensatz %d' % (i+1)
+                if i==0:
+                    st.markdown('### Grad der Polynome')
+                grad = st.slider(bezeichnung_grad, 1, 5, 1)
+                model=np.polyfit(x_werte,y_werte,grad)
+                model_f=np.poly1d(model)
+                ax.plot(x_s,model_f(x_s))
+                
+                f = 0*x
+                zaehler = 0
+
+                for zaehler in range(len(model)):
+                    summand = sp.parse_expr("%f * x**(%d)" % (model[zaehler], len(model)-zaehler-1))
+                    f += summand
+                funktionen.append(r'f_{%d}(x) = ' % (i+1) + sp.latex(f))
+
+            elif anpassung == "Exponentialfunktion":
+                # Fit the function a * np.exp(b * t) to x and y
+                popt, pcov = curve_fit(lambda t, a, b, c: a * np.exp(b * t) + c, x_werte, y_werte)
+                # Extract the optimised parameters
+                a = popt[0]
+                b = popt[1]
+                c = popt[2]
+                x_fitted_curve_fit = np.linspace(np.min(x_werte), np.max(x_werte), 100)
+                y_fitted_curve_fit = a * np.exp(b * x_fitted_curve_fit) + c
+                ax.plot(x_fitted_curve_fit, y_fitted_curve_fit, label=r'curve\_fit')
+                f = np.round(a,3)*sp.exp(np.round(b,3)*x)+np.round(c,3)
+                funktionen.append(r'f_{%d}(x) = ' % (i+1) + sp.latex(f))
+            
+            elif anpassung == "Trigonometrische Funktion":
+                # Fit the function a * np.exp(b * t) to x and y
+                popt, pcov = curve_fit(lambda t, a, b, c, d: a * np.sin(b * (t - c)) + d, x_werte, y_werte)
+                # Extract the optimised parameters
+                a = popt[0]
+                b = popt[1]
+                c = popt[2]
+                d = popt[3]
+                x_fitted_curve_fit = np.linspace(np.min(x_werte), np.max(x_werte), 100)
+                y_fitted_curve_fit = a * np.sin(b * (x_fitted_curve_fit-c)) + d
+                ax.plot(x_fitted_curve_fit, y_fitted_curve_fit, label=r'curve\_fit')
+                f = np.round(a,3)*sp.sin(np.round(b,3)*(x-np.round(c,3)))+np.round(d,3)
+                funktionen.append(r'f_{%d}(x) = ' % (i+1) + sp.latex(f))
+
     st.markdown('''
     ### Funktionsgleichungen
     Funktionsgleichung der Ausgleichskurve
